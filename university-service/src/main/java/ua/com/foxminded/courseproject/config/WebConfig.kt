10747ad202fc0a146ab1c2c0f01418ad1f21626f
Afanasiev Mykhailo <afanasiev.m@ajax.systems>
2023-04-26 15:51:25 +0300

    webconfig changed to kotlin

--- university-service/src/main/java/ua/com/foxminded/courseproject/config/WebConfig.kt
+++ university-service/src/main/java/ua/com/foxminded/courseproject/config/WebConfig.kt
@@ -1,31 +1,30 @@
-package ua.com.foxminded.courseproject.config;
+package ua.com.foxminded.courseproject.config
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.BufferingClientHttpRequestFactory;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.security.config.core.GrantedAuthorityDefaults;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import ua.com.foxminded.courseproject.interceptors.RequestLoggingInterceptors;
-
-import java.util.Collections;
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.BufferingClientHttpRequestFactory
+import org.springframework.http.client.ClientHttpRequestFactory
+import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
+import org.springframework.security.config.core.GrantedAuthorityDefaults
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import ua.com.foxminded.courseproject.interceptors.RequestLoggingInterceptors
 
 @Configuration
 @EnableWebMvc
-public class WebConfig {
-
+open class WebConfig {
     @Bean
-    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
-        return new GrantedAuthorityDefaults("");
+    open fun grantedAuthorityDefaults(): GrantedAuthorityDefaults {
+        return GrantedAuthorityDefaults("")
     }
 
     @Bean
-    public RestTemplate restTemplate(RequestLoggingInterceptors requestLoggingInterceptors) {
-        ClientHttpRequestFactory factory = new BufferingClientHttpRequestFactory(new HttpComponentsClientHttpRequestFactory());
-        RestTemplate restTemplate = new RestTemplate(factory);
-        restTemplate.setInterceptors(Collections.singletonList(requestLoggingInterceptors));
-        return restTemplate;
+    open fun restTemplate(requestLoggingInterceptors: RequestLoggingInterceptors): RestTemplate {
+        val factory: ClientHttpRequestFactory =
+            BufferingClientHttpRequestFactory(HttpComponentsClientHttpRequestFactory())
+        val restTemplate = RestTemplate(factory)
+        restTemplate.interceptors = listOf<ClientHttpRequestInterceptor>(requestLoggingInterceptors)
+        return restTemplate
     }
 }
